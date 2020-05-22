8cae49eefb57f5622ed53377d3423a5fe4dbe812
pi@bleobox.local <pi@bleobox.local>
2020-05-22 22:29:55 +0100

    let's stop using the raspberry pi to commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+chown -R pleroma /etc/pleroma
+chmod -R 777 /etc/pleroma
+chown -R pleroma /var/lib/pleroma
+chmod -R 777 /var/lib/pleroma
+chown -R postgres /var/lib/postgresql
+chmod -R 700 /var/lib/postgresql
 service postgresql start
 sudo -u pleroma /bin/bash /entrypoint.sh
